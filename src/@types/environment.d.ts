d73fa0e466f5aaaa8e7e67ed24182f353354839b
Guilherme Sandi <guilherme_sandi@hotmail.com>
2022-10-17 15:24:38 -0300

    Configure Prismic and Header Links

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -8,5 +8,7 @@ declare namespace NodeJS {
     GITHUB_CLIENT_ID: string;
     GITHUB_CLIENT_SECRET: string;
     FAUNADB_KEY: string;
+    PRISMIC_ENDPOINT: string;
+    PRISMIC_ACCESS_TOKEN: string;
   }
 }
